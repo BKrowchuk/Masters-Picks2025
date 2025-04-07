aa0ae93569c9aabc5ce5d044472a20ba5c9bd46e
benkrowchuk <162325063+benkrowchuk@users.noreply.github.com>
2025-04-07 13:11:31 -0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.csv']
+  assetsInclude: ['**/*.csv'],
+  base: '/Master2025-Picks/'
 })
